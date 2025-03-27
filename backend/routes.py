3f7fe9f24ae89285ee14f0ad7fa1fff6d9526d9a
Smalls141414 <ari.grady14@gmail.com>
2025-03-27 00:52:53 -0700

    FrontEnd Midterm Finalization

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, request, jsonify, session
 
 def define_routes(app):
-    @app.route("/test", methods=['GET', 'POST'])
-    def test():
-	    return "Heelo, World!"
+    @app.route("/retire", methods=['GET', 'POST'])
+    def retire():
+        data = request.get_json()
+        print(data)
+        testResponse = {'retire_amt': 5000, 'retire_years': 5, 'inflate_rate': 12, 'time_period': 5, 'retire_amt_raw': 5000}
+        return jsonify(testResponse)
+
+    @app.route("/portfolio", methods=['GET', 'POST'])
+    def portfolio():
+        data = request.get_json()
+        print(data)
+        testResponse = {'ticker': 'GOOGL', 'avg_price': 12, 'end_price': 13, 'abs_return': 14, 'amount_inv': 15, 'profit': 16, 'value': 17, 'stocks': 18, 'today_value': 19, 'final_profit': 20, 'final_return': 21, 'ann_return': 22}
+        return jsonify(testResponse)
--- stock-app/src/app/page.js
+++ stock-app/src/app/page.js
@@ -5,12 +5,56 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function Home() {
-  const [test, setTest] = useState('');
+  const [retireResult, setRetireResult] = useState([]);
+  const [portResult, setPortResult] = useState([]);
+  const [expend, setExpend] = useState('');
+  const [currentAge, setCurrentAge] = useState('');
+  const [retireAge, setRetireAge] = useState('');
+  const [ticker, setTicker] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [investment, setInvestment] = useState('');
 
-  const testResponse = async(e) => {
+  // Retirement Calculator Form
+  const handleRetirementCalc = async(e) => {
     e.preventDefault();
-    const response = await axios.get('http://localhost:5000/test', test);
-    setTest(response.data);
+
+    const retireForm = {
+      expend: expend,
+      current_age: currentAge,
+      retire_age: retireAge
+    }
+
+    // Get & Set Results
+    const response = await axios.post('http://localhost:5000/retire', retireForm);
+    setRetireResult(response.data);
+
+    // Display Results
+    document.getElementById("calcresult1").style.display = 'block';
+    document.getElementById("calcresult2").style.display = 'block';
+  }
+
+  // Portfolio Value Estimation Form
+  const handlePortfolio = async(e) => {
+    e.preventDefault();
+
+    const portForm = {
+      ticker: ticker,
+      start_date: startDate,
+      end_date: endDate,
+      investment: investment,
+      frequency: frequency
+    }
+
+    // Get & Set Results
+    const response = await axios.post('http://localhost:5000/portfolio', portForm);
+    setPortResult(response.data);
+
+    // Display Results
+    document.getElementById("portresult1").style.display = 'block';
+    document.getElementById("portresult2").style.display = 'block';
+    document.getElementById("portresult3").style.display = 'block';
   }
 
   return (
@@ -21,34 +65,42 @@ export default function Home() {
 
         <div className={styles.retirementcalc}>
           <h2>Retirement Calculator</h2>
-          <form>
+          <form onSubmit={handleRetirementCalc}>
             <p>Annual Expenditure($):</p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={expend} onChange={(e)=> setExpend(e.target.value)}/>
             <p>Current Age: </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={currentAge} onChange={(e)=> setCurrentAge(e.target.value)}/>
             <p>Planned Retirement Age: </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={retireAge} onChange={(e)=> setRetireAge(e.target.value)}/>
 
-            <input type="button" onClick={testResponse} className={styles.tertbutton} value="Calculate"/>
+            <input type="submit" className={styles.tertbutton} value="Calculate"/>
           </form>
+          <p id="calcresult1" className={styles.result}> You would need to have saved a total of ${retireResult.retire_amt} to retire comfortably {retireResult.retire_years} years from today (at the time of retirement). This amount is adjusted for inflation of {retireResult.inflate_rate}% based on the average annual inflation in the United States over the past {retireResult.time_period} years and reflects the future value, not the amount you need to have saved today</p>
+          <p id="calcresult2" className={styles.result}> For context, the amount would be worth ${retireResult.retire_amt_raw} today </p>
         </div>
 
         <div className={styles.portfolioest}>
           <h2>Portfolio Value Estimator</h2>
-          <form>
+          <form onSubmit={handlePortfolio}>
             <p>Ticker Symbol:</p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={ticker} onChange={(e)=> setTicker(e.target.value)}/>
             <p>Start Date (YYYY-MM-DD): </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={startDate} onChange={(e)=> setStartDate(e.target.value)}/>
             <p>End Date (YYYY-MM-DD): </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={endDate} onChange={(e)=> setEndDate(e.target.value)}/>
             <p>Frequency Daily(d), Monthly(m), or Yearly(y): </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={frequency} onChange={(e)=> setFrequency(e.target.value)}/>
             <p>Investment($): </p>
-            <input type="text" className={styles.textfield} />
+            <input type="text" className={styles.textfield} value={investment} onChange={(e)=> setInvestment(e.target.value)}/>
 
-            <input type="button" onClick={testResponse} className={styles.secbutton} value="Calculate"/>
+            <input type="submit" className={styles.secbutton} value="Calculate"/>
           </form>
+          <p id="portresult1" className={styles.result}> Your portfolio of {portResult.stocks} stocks would be valued at ${portResult.value} with a profit of ${portResult.profit}. </p>
+          <p id="portresult2" className={styles.result}> For context, the average price of {portResult.ticker} during the time period was ${portResult.avg_price}, and the end price was ${portResult.end_price}. Based on daily dollar cost averaging, the absolute rate of return was {portResult.abs_return}% </p>
+          <p id="portresult3" className={styles.result}> If you held onto the stocks until today, your portfolio value would have been: ${portResult.today_value} with a net growth of ${portResult.final_profit} which is {portResult.final_return}% of total growth with an annualized rate of return of {portResult.ann_return}%</p>
+        </div>
+        <div className={styles.temp}>
+          <p></p>
         </div>
     </div>
   );
